f77baccaae6746759c71280b46de691b898cc887
Nasruddinkhan <nasruddinkhan44@gmail.com>
2023-02-09 00:56:29 +0530

    add client registration jpa

--- social-authorizationserver/src/main/java/com/mypractice/microservice/oauthserver/config/AuthorizationServerConfiguration.java
+++ social-authorizationserver/src/main/java/com/mypractice/microservice/oauthserver/config/AuthorizationServerConfiguration.java
@@ -9,18 +9,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -34,30 +26,7 @@ import java.util.UUID;
 @Configuration
 @AllArgsConstructor
 public class AuthorizationServerConfiguration {
-    private final PasswordEncoder passwordEncoder;
-	@Bean
-	public RegisteredClientRepository registeredClientRepository(JdbcTemplate jdbcTemplate) {
-		
-		RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-				.clientId("client1")
-				.clientSecret(passwordEncoder.encode("myClientSecretValue"))
-				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-				.redirectUri("http://127.0.0.1:8081/login/oauth2/code/users-client-oidc")
-				.redirectUri("http://127.0.0.1:8081/authorized")
-				.scope(OidcScopes.OPENID)
-				.scope("read")
-				//.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-				.build();
 
-        final var registeredClientRepository = new JdbcRegisteredClientRepository(jdbcTemplate);
-        registeredClientRepository.save(registeredClient);
-        return registeredClientRepository;
-	}
-	
-	
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -101,8 +70,4 @@ public class AuthorizationServerConfiguration {
         return keyPairGenerator.generateKeyPair();
     }
 
-
-
-
-	
 }
---
+++ social-authorizationserver/src/main/java/com/mypractice/microservice/oauthserver/entity/Client.java
@@ -0,0 +1,42 @@
+package com.mypractice.microservice.oauthserver.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "`client`")
+public class Client {
+    @Id
+    @Column(name = "id", nullable = false)
+    private String  id;
+
+    private String clientId;
+    private Instant clientIdIssuedAt;
+    private String clientSecret;
+    private Instant clientSecretExpiresAt;
+    private String clientName;
+    @Column(length = 1000)
+    private String clientAuthenticationMethods;
+    @Column(length = 1000)
+    private String authorizationGrantTypes;
+    @Column(length = 1000)
+    private String redirectUris;
+    @Column(length = 1000)
+    private String scopes;
+    @Column(length = 2000)
+    private String clientSettings;
+    @Column(length = 2000)
+    private String tokenSettings;
+
+
+}
